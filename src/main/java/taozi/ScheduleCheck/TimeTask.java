f5b10e8e7d9a51976d37d0f40951e5f8517aa3b2
S9037218 <Orangejuice135!>
2021-06-08 20:41:58 +0800

    first Commit

--- src/main/java/taozi/ScheduleCheck/TimeTask.java
+++ src/main/java/taozi/ScheduleCheck/TimeTask.java
@@ -1,9 +1,13 @@
 package taozi.ScheduleCheck;
 
+import com.alibaba.fastjson.JSON;
+import taozi.TTApi.TTPost;
+import taozi.TTApi.TTRequest;
 import taozi.branch.BranchInformation;
 import taozi.branch.MasterInformation;
 import taozi.verifier.TimeIntervalVerifier;
 
+import java.util.Collections;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -19,9 +23,19 @@ public class TimeTask {
                 Long masterTime = MasterInformation.getMasterTime(url);
                 System.out.println("brandTime: " + brandTime + "\t" + "masterTime: " + masterTime);
                 System.out.println(TimeIntervalVerifier.ifNeedWarn(brandTime, masterTime));
+                TTPost.doPostJson("http://msg.ads.oppo.local/api/tt_push", JSON.toJSONString(getTTRequest()));
             }
         }, 0, taskInterval);
 
     }
+
+    private static TTRequest getTTRequest() {
+        TTRequest ttRequest = new TTRequest();
+        ttRequest.setMsg("taozi");
+        ttRequest.setTo_user_list(Collections.singletonList("S9037218"));
+        ttRequest.setBizAlarm("sjsj");
+        return ttRequest;
+    }
+
 
 }
