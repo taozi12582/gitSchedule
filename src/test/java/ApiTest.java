6f964fb19f94c8b6d80eaf2d543db04286751660
S9037218 <Orangejuice135!>
2021-06-08 20:23:41 +0800

    first Commit

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,8 +1,16 @@
 
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import taozi.TTApi.TTRequest;
 
 import java.io.*;
@@ -14,48 +22,50 @@ import java.util.Collections;
 public class ApiTest {
 
 
-    public static String doPost(String url, JSONObject json){
-        HttpClient httpClient = new HttpClient();
-        PostMethod postMethod = new PostMethod(url);
-
-        postMethod.addRequestHeader("accept", "*/*");
-        postMethod.addRequestHeader("connection", "Keep-Alive");
-        //设置json格式传送
-        postMethod.addRequestHeader("Content-Type", "application/json;charset=utf-8");
-        //必须设置下面这个Header
-        postMethod.addRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36");
-        //添加请求参数
-        postMethod.addParameter("msg", json.getString("msg"));
-        postMethod.addParameter("to_user_list",json.getString("to_user_list"));
-
-        String res = "";
+    public static String doPostJson(String url, String json) {
+        // 创建Httpclient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        String resultString = "";
         try {
-            int code = httpClient.executeMethod(postMethod);
-            System.out.println(code);
-            if (code == 200){
-                System.out.println("200");
-                res = postMethod.getResponseBodyAsString();
-                System.out.println(res);
-            }
-        } catch (IOException e) {
+            // 创建Http Post请求
+            HttpPost httpPost = new HttpPost(url);
+            // 创建请求内容
+            StringEntity entity;
+            entity = new StringEntity(json, ContentType.APPLICATION_JSON);
+            httpPost.setEntity(entity);
+            // 执行http请求
+            response = httpClient.execute(httpPost);
+            resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        return res;
+        return resultString;
     }
+
 
     public static void main(String[] args) {
 //        doGet("http://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=13026194071", "UTF-8");
-        System.out.println("-----------分割线------------");
-        System.out.println("-----------分割线------------");
-        System.out.println("-----------分割线------------");
+//        System.out.println("-----------分割线------------");
+//        System.out.println("-----------分割线------------");
+//        System.out.println("-----------分割线------------");
+//
+//        JSONObject jsonObject = new JSONObject();
+//        jsonObject.put("msg", "taozi");
+//        jsonObject.put("to_user_list", Collections.singletonList("S9037218"));
 
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("msg", "taozi");
-        jsonObject.put("to_user_list",Collections.singletonList("S9037218"));
-        doPost("http://commerce-ads-test.wanyol.com/sms/api/tt_push", jsonObject);
+        System.out.println(JSON.toJSONString(getTTRequest()));
+        String s = doPostJson("http://commerce-ads-test.wanyol.com/sms/api/tt_push", JSON.toJSONString(getTTRequest()));
+        System.out.println(s);
     }
 
-    private static TTRequest getTTRequest(){
+    private static TTRequest getTTRequest() {
         TTRequest ttRequest = new TTRequest();
         ttRequest.setMsg("taozi");
         ttRequest.setTo_user_list(Collections.singletonList("S9037218"));
