a6bbf80f0ac565a62167d8c469bc126478b35fb9
S9037218 <Orangejuice135!>
2021-06-09 12:36:13 +0800

    first Commit

--- src/main/java/config/DetailConfiguration.java
+++ src/main/java/config/DetailConfiguration.java
@@ -1,6 +1,6 @@
-package taozi.config;
+package config;
 
-public class Configuration {
+public class DetailConfiguration {
     public static final String branchName = "release";
     public static final String masterName = "origin/master";
     public static final String repositoryDir = "D:\\ExampleCode\\hiworld\\.git";
